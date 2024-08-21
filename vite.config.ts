d9083db64054eb9362588f789c25216c0ed537d3
wzwz5200 <e1qasdfttyg@outlook.com>
2024-08-21 16:31:26 +0800

    fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,28 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+    
+    
+  },
+  server: {
+    // proxy: {
+    //   '/proxy_url': {
+    //     target: 'http://127.0.0.1:8888',
+    //     // secure: false, // 请求是否为https
+    //     changeOrigin: true,
+    //     rewrite:(path)=>path.replace(/^\/proxy_url/,'') //api替换为'',
+    //   },
+    // },
+   
+    host: '0.0.0.0'
+   
+    
+  },
+
+  
 })
