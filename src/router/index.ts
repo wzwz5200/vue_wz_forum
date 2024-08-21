c4d7da716819f4b14f3a27295dad7dbb9ec087e0
wzwz5200 <e1qasdfttyg@outlook.com>
2024-08-21 17:07:10 +0800

    add game page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import Game from '@/views/thems/Game.vue'
+import Hardware from '@/views/thems/hardware.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +10,31 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      children: [
+        {
+          // 当 /user/:id/profile 匹配成功
+          // UserProfile 将被渲染到 User 的 <router-view> 内部
+          path: '',
+          component: AboutView,
+        },
+    
+        {
+          // 当 /user/:id/profile 匹配成功
+          // UserProfile 将被渲染到 User 的 <router-view> 内部
+          path: '',
+          name: 'Game',
+          component: Game,
+        },
+        {
+          // 当 /user/:id/profile 匹配成功
+          // UserProfile 将被渲染到 User 的 <router-view> 内部
+          path: 'h',
+          name: 'Hardware',
+          component: Hardware,
+        },
+    
+      ],
     },
     {
       path: '/about',
@@ -16,8 +43,13 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    
+    
+    
+    
   ]
 })
 
 export default router
+
